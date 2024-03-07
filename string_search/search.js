eb54895c2844bdfe2c7b75e1cc90283ee2c64781
Anty <sanjana.anty@gmail.com>
2024-03-07 14:13:16 +0600

    String search methods

---
+++ string_search/search.js
@@ -0,0 +1,48 @@
+//Searching position of a word in string
+/*let str = "Please locate the word 'locate' here";
+let pos = str.indexOf("locate");
+console.log(pos);*/
+
+/*let str = "Please locate the word 'locate' here";
+let pos = str.indexOf("Please");
+console.log(pos);*/
+
+/*let str = "Please locate the word 'locate' here";
+let pos = str.indexOf("please");
+console.log(pos);*/
+
+/*let str = "Please locate the word 'locate' here";
+let pos = str.lastIndexOf("locate");
+console.log(pos);*/
+
+/*let str = "Please locate the word 'locate' here";
+let pos = str.indexOf("locate", 8); //search starts from the beginning
+console.log(pos);*/
+
+/*let str = "Please locate the word 'locate' here";
+let pos = str.lastIndexOf("locate", 15); //search starts from the 15th position of the string
+console.log(pos);*/
+
+/*let str = "Please locate the word 'locate' here";
+let pos = str.search("locate"); //search is more powerful than indexof
+console.log(pos);*/
+
+/*let str = "Hello world!, welcome to the universe";
+let pos = str.includes("world"); //returns true if the string is present
+console.log(pos);*/
+
+/*let str = "Hello world!, welcome to the universe";
+let pos = str.includes("world", 12); //search starts from 12
+console.log(pos);*/
+
+/*let str = "Hello world!, welcome to the universe";
+let pos = str.startsWith("world");
+console.log(pos);*/
+
+let str = "Hello world!, welcome to the universe";
+let pos = str.endsWith("world", 11); //second parameter defines the end of search
+console.log(pos);
+
+
+
+
