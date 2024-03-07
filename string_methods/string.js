1cac4b14c60b2a55e5c7f91528bfea1f3ea62543
Anty <sanjana.anty@gmail.com>
2024-03-07 13:45:13 +0600

    String methods

---
+++ string_methods/string.js
@@ -0,0 +1,127 @@
+//Character Extracting Methods
+/*let text = "Hello World"
+let char = text.charAt(6)
+console.log(char)*/
+
+/*let text = "Hello World"
+let char = text.charCodeAt(6)
+console.log(char)*/
+
+/*const name = "W3schools"
+let char = name.at(3)
+console.log(char)*/
+
+/*const name = "W3schools"
+let char = name.at(-2)
+console.log(char)*/
+
+/*let text = "Hello World!"
+let char = text[3]
+console.log(char)*/
+
+//String parts extracting method
+/*let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.slice(7);
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.slice(-12);
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.slice(-12, -1);
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.substring(-12, -1); nagetive values are treated as 0 here
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.substr(7, 6);
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.substr(7);
+console.log(part)*/
+
+/*let text = "Apple, Banana, Kiwi";
+let part = text.substr(-4);
+console.log(part)*/
+
+//Coverting to upper and lower case
+/*let text = "hello world!";
+let upper = text.toUpperCase();
+console.log(upper);*/
+
+/*let text = "HELLO WORLD!";
+let upper = text.toLowerCase();
+console.log(upper)*/
+
+///string concat
+/*let text1 = "Hello";
+let text2 = "World!";
+let text3 = text1.concat(" ", text2);
+console.log(text3)*/
+
+/*let text1 = "Hello".concat(" " + "World!");
+console.log(text1);*/
+
+//String trim methods
+/*let text1 = "      Hello World!       ";
+let text2 = text1.trim();
+console.log(text2);*/
+
+/*let text1 = "      Hello World!       ";
+let text2 = text1.trimStart();
+console.log(text2);*/
+
+/*let text1 = "      Hello World!       ";
+let text2 = text1.trimEnd();
+console.log(text2);*/
+
+//String Padding
+/*let text1 = "5";
+let text2 = text1.padStart(4, "x");
+console.log(text2);*/
+
+/*let text1 = "5";
+let text2 = text1.padEnd(4, "x");
+console.log(text2);*/
+
+/*let num = 5;
+let text1 = num.toString();
+let text2 = text1.padStart(4, "x");
+console.log(text2);*/
+
+//string repeat
+/*let text1 = "Hello World!";
+let text2 = text1.repeat(6);
+console.log(text2);*/
+
+//string replace
+/*let text = "Hello World and World!";
+let text1 = text.replace("World", "Dolly");
+console.log(text1);*/
+
+/*let text = "I love cats. Cats are very easy to love. Cats are adorable.";
+text = text.replaceAll("Cats", "Dogs");
+text = text.replaceAll("cats", "dogs");
+console.log(text);*/
+
+//String to Array
+/*let text = "I love cats. Cats are very easy to love. Cats are adorable.";
+let myArr = text.split(" ");
+console.log(myArr);*/
+
+/*let text = "I love cats. Cats are very easy to love. Cats are adorable.";
+let myArr = text.split(""); // creats single element array
+console.log(myArr);*/
+
+/*let text = "I love cats. Cats are very easy to love. Cats are adorable.";
+let myArr = text.split(); //The whole string is in myArr[0]
+console.log(myArr);*/
+
---
+++ strings/string.js
@@ -0,0 +1,25 @@
+// let text = `He's called "John"`;
+// console.log(text);
+
+
+// Use of typeof 
+/*let x = "John";
+let y = new String("John");
+
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(x==y)
+console.log(x===y)*/
+
+// Use of length 
+/*let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length)*/
+
+// Use of backslash Escape Character
+let text = "We,re the so-called \"Vikings\" conquering the world!"
+let text1 = 'It\'s alright'
+let text2 = "The \\ character is called backslash"
+console.log(text)
+console.log(text1)
+console.log(text2)
