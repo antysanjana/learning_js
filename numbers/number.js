78b2fbf24d818f4f5df7854aa80ab8a324b8fba3
Anty <sanjana.anty@gmail.com>
2024-03-07 19:23:21 +0600

    Number in JS

---
+++ numbers/number.js
@@ -0,0 +1,96 @@
+//extra large and extra small numbers
+/*let x = 123e5;
+let y = 123e-5;
+console.log(x);
+console.log(y);*/
+
+//Floating point arithmatic
+/*let x = 0.2;
+let y = 0.1;
+let z = x + y; //gives wrong result
+console.log(z);*/
+
+/*let x = (0.2 * 10 + 0.1 * 10)/10; //give accurate result
+console.log(x);*/
+
+//Adding numbers and strings
+/*let x = "The result is " + 10 + 20;
+console.log(x);*/
+
+//Numeric strings don't work for + operator
+/*let x = "100";
+let y = "10";
+let z = x/y;   //x and y behave as numbers
+console.log(z)*/
+
+/*let x = "100";
+let y = "10";
+let z = x * y;   //x and y behave as numbers
+console.log(z)*/
+
+/*let x = "100";
+let y = "10";
+let z = x - y;   //x and y behave as numbers
+console.log(z)*/
+
+/*let x = "100";
+let y = "10";
+let z = x + y;   //x and y behave as strings
+console.log(z)*/
+
+//NaN
+/*let x = "Apple";
+let y = 10;
+console.log(x / y);*/
+
+/*let x = "100";
+let y = 10;
+console.log(x / y); //result will be number*/
+
+/*let x = 10 / "Apple";
+console.log(isNaN(x));*/
+
+/*let x = NaN;
+let y = 10;
+console.log(x + y);*/
+
+/*let x = NaN;
+let y = "10";
+console.log(x + y); //result will be a string*/
+
+//Infinity in JS
+/*let num = 2;
+
+while(num != Infinity){
+    num *= num;
+    console.log(num);
+}*/
+
+/*let x = 2 / 0;
+let y = -2 / 0;
+console.log(x , y);*/
+
+//Number System
+/*let x = 0xFF;
+console.log(x);*/
+
+/*let num = 32;
+console.log(num.toString(10));
+console.log(num.toString(2)); //toString changes the base of the number
+console.log(num.toString(8));
+console.log(num.toString(16));
+console.log(num.toString(32));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
