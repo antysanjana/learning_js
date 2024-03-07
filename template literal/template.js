a42ea59d1ac27e55ba32205ad59a95e3ee4f856c
Anty <sanjana.anty@gmail.com>
2024-03-07 15:37:26 +0600

    Use of template string

---
+++ template literal/template.js
@@ -0,0 +1,27 @@
+//String interpolation
+/*let firstName = "John";
+let lastName = "Doe";
+let fullName = `Welcome, ${firstName} ${lastName}`;
+console.log(fullName);*/
+
+/*let price = 10;
+let VAT = 0.25;
+let total = `Total: ${price * (1+VAT)}`
+console.log(total);*/
+
+/*let header = "Template Strings";
+let tags = ["template strings", "javascript", "es6"];
+
+let html = `<h2>${header}</h2><ul>`
+for(const x of tags){
+    html += `<li>${x}</li>`;
+}
+
+html += `</ul>`;
+console.log(html);*/
+
+
+
+
+
+
