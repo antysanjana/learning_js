0c2583fa216a455dfdf8054a87a83a7d6f520521
Anty <sanjana.anty@gmail.com>
2024-03-10 11:00:55 +0600

    Number Properties

---
+++ number_properties/number.js
@@ -0,0 +1,31 @@
+//EPSILON
+/*let x = Number.EPSILON;
+console.log(x);*/
+
+//MAX_VALUE
+// let x = Number.MAX_VALUE;
+// console.log(x);
+
+//MIN_VALUE
+/*let x = Number.MIN_VALUE;
+console.log(x);*/
+
+//Number properties are only applicable to number variables
+/*let x =6;
+console.log(x.MAX_VALUE); //gives an error*/
+
+//MAX and MIN SAFE INTEGER
+/*let x = Number.MAX_SAFE_INTEGER;
+console.log(x);
+let y = Number.MIN_SAFE_INTEGER;
+console.log(y);*/
+
+//INFINITY
+let x = Number.POSITIVE_INFINITY;
+console.log(x);
+let y = Number.NEGATIVE_INFINITY;
+console.log(y);
+
+//NaN
+let p = Number.NaN;
+console.log(p);
