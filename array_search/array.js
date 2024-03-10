eefc83fe067dfa1b4b75132b93b05be622414e89
Anty <sanjana.anty@gmail.com>
2024-03-10 15:06:38 +0600

    Array search methods

---
+++ array_search/array.js
@@ -0,0 +1,48 @@
+//JavaScript Array indexOf()
+/*const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let pos = fruits.indexOf("Apple") + 1;
+console.log(pos);*/
+
+//JavaScript Array lastIndexOf()
+/*const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let pos = fruits.lastIndexOf("Orange", -1) + 1;
+console.log(pos);*/
+
+//JavaScript Array includes()
+/*const fruits = ["Apple", "Orange", "Apple", "Mango"];
+console.log(fruits.includes("Mango"));*/
+
+//JavaScript Array find()
+/*const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunc);
+console.log(first);
+
+function myFunc(value, index, array){
+    return value > 18;
+}*/
+
+//JavaScript Array findIndex()
+/*const numbers = [4, 9, 16, 25, 29];
+let first = numbers.findIndex(myFunc);
+console.log(first);
+
+function myFunc(value, index, array){
+    return value > 18;
+
+}*/
+
+//JavaScript Array findLast() Method
+/*const temp = [27, 28, 30, 40, 42, 35, 30];
+let high = temp.findLast(x => x > 40);
+console.log(high);*/
+
+//JavaScript Array findLastIndex() Method
+const temp = [27, 67, 30, 40, 42, 35, 30];
+let high = temp.findLastIndex(x => x > 40);
+console.log(high);
+
+
+
+
+
+
