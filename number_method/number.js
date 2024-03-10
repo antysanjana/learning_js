9b676a38f2fe12233a76e31e16dd8c9e275e80e8
Anty <sanjana.anty@gmail.com>
2024-03-10 10:45:46 +0600

    Number methods

---
+++ number_method/number.js
@@ -0,0 +1,93 @@
+//toString method
+/*let x = 123;
+y = x.toString();
+console.log(x.toString());
+console.log((123).toString());
+console.log((100+23).toString());
+console.log(typeof y);*/
+
+//The toExponential() Method
+/*let x = 9.565;
+console.log(x.toExponential(2));
+console.log(x.toExponential(4));
+console.log(x.toExponential(6));*/
+
+//The toFixed() Method
+/*let x = 9.565;
+console.log(x.toFixed(0));
+console.log(x.toFixed(2));
+console.log(x.toFixed(4));
+console.log(x.toFixed(6));*/
+
+//toPrecision method
+/*let x = 9.565;
+console.log(x.toPrecision(2));
+console.log(x.toPrecision(4));
+console.log(x.toPrecision(6));*/
+
+//valueOf() method
+/*let x = 123;
+console.log(x.valueOf());
+console.log((123).valueOf());
+console.log((100 + 23).valueOf());*/
+
+//Converting Variables to Numbers
+//Number method
+/*console.log(Number(true));
+console.log(Number(false));
+console.log(Number("10"));
+console.log(Number("   10"));
+console.log(Number("10   "));
+console.log(Number("10.03"));
+console.log(Number("     10      "));
+console.log(Number("10,33"));
+console.log(Number("10 33"));
+console.log(Number("John"));
+
+console.log(Number(new Date("1970-02-01")));*/
+
+//parseInt method
+/*console.log(parseInt("-10"));
+console.log(parseInt("-10.33"));
+console.log(parseInt("10.33"));
+console.log(parseInt("10 20 30"));
+console.log(parseInt("10 years"));
+console.log(parseInt("years 10"));*/
+
+//parseFloat method
+/*console.log(parseFloat("-10"));
+console.log(parseFloat("-10.33"));
+console.log(parseFloat("10.33"));
+console.log(parseFloat("10 20 30"));
+console.log(parseFloat("years 10"));*/
+
+//Number Object Methods
+/*console.log(Number.isInteger(10));
+console.log(Number.isInteger(10.05));
+
+console.log(Number.isSafeInteger(10));
+console.log(Number.isSafeInteger(12345678901234567890));*/
+
+/*console.log(Number.parseFloat("10"));
+console.log(Number.parseFloat("10.33"));
+console.log(Number.parseFloat("10 20 30"));
+console.log(Number.parseFloat("10 years"));
+console.log(Number.parseFloat("years 10"));*/
+
+console.log(Number.parseFloat("10"));
+console.log(Number.parseFloat("10.33"));
+console.log(Number.parseFloat("-10.33"));
+console.log(Number.parseFloat("10 20 30"));
+console.log(Number.parseFloat("10 years"));
+console.log(Number.parseFloat("years 10"));
+
+
+
+
+
+
+
+
+
+
+
