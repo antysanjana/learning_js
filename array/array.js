9574f581a11e25e379441822affa7cbb627d4364
Anty <sanjana.anty@gmail.com>
2024-03-10 12:35:04 +0600

    Array Basics

---
+++ array/array.js
@@ -0,0 +1,79 @@
+//Accessing Array Elements
+/*let cars = ["Saab", "Volvo", "BMW"];
+let car = cars[2];
+console.log(car);*/
+
+//Changing an Array Element
+/*let cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+console.log(cars[0]);*/
+
+//Converting an Array to a String
+// let fruits = ["Banana", "Apple", "Orange", "Mango"];
+// console.log(fruits.toString());
+
+//Access the Full Array
+/*let fruits = ["Banana", "Apple", "Orange", "Mango"];
+console.log(fruits);*/
+
+//Arrays are Objects
+/*let fruits = ["Banana", "Apple", "Orange", "Mango"];
+console.log(typeof fruits);*/
+
+//The length Property
+/*let fruits = ["Banana", "Apple", "Orange", "Mango"];
+console.log(fruits.length)*/
+
+//Accessing the Last Array Element
+/*let fruits = ["Banana", "Apple", "Orange", "Mango"];
+let fruit = fruits[fruits.length - 1]; 
+console.log(fruit)*/
+
+//Looping Array Elements
+/*const fruits = ["Banana", "Apple", "Orange", "Mango"];
+let flen = fruits.length;
+
+for(i = 0; i < flen; i++){
+    console.log(fruits[i]);
+}*/
+
+//forEach technique
+/*const fruits = ["Banana", "Apple", "Orange", "Mango"];
+
+fruits.forEach(myFunction);
+
+function myFunction(value) {
+    console.log(value);
+}*/
+
+//Adding Array Elements
+/*const fruits = ["Banana", "Apple", "Orange", "Mango"];
+fruits.push("Lemon");
+console.log(fruits);*/
+
+/*const fruits = ["Banana", "Apple", "Orange", "Mango"];
+fruits[fruits.length] = "Lemon";
+console.log(fruits);*/
+
+//JavaScript new Array()
+/*const points = new Array(40, 100, 1, 5, 25, 10);
+console.log(points);*/
+
+//How to Recognize an Array
+const fruits = ["Banana", "Apple", "Orange", "Mango"];
+//console.log(Array.isArray(fruits));
+console.log(fruits instanceof Array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
