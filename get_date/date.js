11efd46efe56b6497c3bbdba6d93dff1626efc67
Anty <sanjana.anty@gmail.com>
2024-03-11 14:19:20 +0600

    GET Date methods

---
+++ get_date/date.js
@@ -0,0 +1,102 @@
+//The getFullYear() Method
+/*const d = new Date("2021-03-25");
+console.log(d.getFullYear());*/
+
+/*const d = new Date();
+console.log(d.getFullYear());*/
+
+//The getMonth() Method
+/*const d = new Date("2021-03-25");
+console.log(d.getMonth() + 1);*/
+
+/*const d = new Date();
+console.log(d.getMonth() + 1);*/
+
+/*const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const d = new Date();
+let month = months[d.getMonth()];
+console.log(month);*/
+
+/*const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const d = new Date("2021-05-25");
+let month = months[d.getMonth()];
+console.log(month);*/
+
+//The getDate() Method
+/*const d = new Date("2021-03-15");
+console.log(d.getDate());*/
+
+/*const d = new Date();
+console.log(d.getDate());*/
+
+//The getHours() Method
+/*const d = new Date("2021-03-15");
+console.log(d.getHours());*/
+
+/*const d = new Date();
+console.log(d.getHours());*/
+
+//The getMinutes() Method
+/*const d = new Date("2021-03-15");
+console.log(d.getMinutes());*/
+
+/*const d = new Date();
+console.log(d.getMinutes());*/
+
+//The getSeconds() Method
+/*const d = new Date("2021-03-15");
+console.log(d.getSeconds());*/
+
+/*const d = new Date();
+console.log(d.getSeconds());*/
+
+//The getMilliseconds() Method
+/*const d = new Date("2021-03-15");
+console.log(d.getMilliseconds());*/
+
+/*const d = new Date();
+console.log(d.getMilliseconds());*/
+
+//The getDay() Method
+/*const d = new Date("2021-03-15");
+console.log(d.getDay());*/
+
+/*const d = new Date();
+console.log(d.getDay());*/
+
+/*const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date("2021-03-15");
+let date = days[d.getDay()];
+console.log(date);*/
+
+/*const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+let date = days[d.getDay()];
+console.log(date);*/
+
+//The getTime() Method
+/*const d = new Date("1970-01-01");
+console.log(d.getTime());*/
+
+/*const d = new Date();
+console.log(d.getTime());*/
+
+/*const d = new Date("2015-03-15");
+console.log(d.getTime());*/
+
+//The Date.now() Method
+/*let ms = Date.now();
+console.log(ms);*/
+
+//The getTimezoneOffset() Method
+const d = new Date();
+let diff = d.getTimezoneOffset();
+console.log(diff);
+
+
+
+
+
+
