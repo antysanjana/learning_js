c8f68e5aabb5703406754fdd4c6116d727becf1c
Anty <sanjana.anty@gmail.com>
2024-03-11 18:11:12 +0600

    random numbers

---
+++ random/random.js
@@ -0,0 +1,46 @@
+//Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive):
+/*console.log(Math.random());*/
+
+//Returns a random integer from 0 to 9
+/*console.log(Math.random() * 10);*/
+
+//Returns a random integer from 0 to 10
+/*console.log(Math.random() * 11);*/
+
+//Returns a random integer from 0 to 99
+/*console.log(Math.random() * 100);*/
+
+//Returns a random integer from 0 to 100
+/*console.log(Math.random() * 101);*/
+
+//Returns a random integer from 1 to 10
+/*let num = Math.floor((Math.random() * 10) + 1);
+console.log(num);*/
+
+//Returns a random integer from 1 to 100
+/*let num = Math.floor((Math.random() * 100) + 1);
+console.log(num);*/
+
+//This JavaScript function always returns a random number between min (included) and max (excluded)
+/*let min = 5;
+let max = 50;
+let num = rnd(min, max);
+console.log(num);
+
+function rnd(min, max){
+    return Math.floor((Math.random() * (max - min)) + min);
+}*/
+
+//This JavaScript function always returns a random number between min and max (both included)
+/*let min = 5;
+let max = 50;
+let num = rnd(min, max);
+console.log(num);
+
+function rnd(min, max){
+    return Math.floor((Math.random() * (max - min)) + (min + 1));
+}*/
+
+
+
+
