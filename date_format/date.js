aacec73c87f157ef9407e4711368a43c29ab88d6
Anty <sanjana.anty@gmail.com>
2024-03-11 13:24:33 +0600

    Date formats

---
+++ date_format/date.js
@@ -0,0 +1,39 @@
+//JavaScript ISO Dates
+/*const d = new Date("2015-03-25");
+console.log(d);*/
+
+//ISO Dates (Year and Month)
+/*const d = new Date("2015-03");
+console.log(d);*/
+
+//ISO Dates (Only Year)
+/*const d = new Date("2015");
+console.log(d);*/
+
+//ISO Dates (Date-Time)
+/*const d = new Date("2015-03-15T12:00:00Z");
+console.log(d);*/
+
+/*const d = new Date("2015-03-15T12:00:00-06:30");
+console.log(d);*/
+
+//JavaScript Short Dates.
+/*const d = new Date("03/25/2015");
+console.log(d);*/
+
+//JavaScript Long Dates.
+/*const d = new Date("25 Mar 2015");
+const d1 = new Date("Mar 25 2015");
+const d2 = new Date("January 24 2015");
+console.log(d);
+console.log(d1);
+console.log(d2);*/
+
+//Date Input - Parsing Dates
+/*let ms = Date.parse("May 12 2015");
+console.log(ms);*/
+
+let ms = Date.parse("May 12 2015");
+const d = new Date(ms);
+console.log(d);
+
