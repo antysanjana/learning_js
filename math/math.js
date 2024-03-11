98c697f465342d382dc2a7508eadc8d1240af72a
Anty <sanjana.anty@gmail.com>
2024-03-11 17:38:58 +0600

    Math method basics

---
+++ math/math.js
@@ -0,0 +1,73 @@
+//console.log(Math.PI);
+
+//Math Properties (Constants)
+/*console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);*/
+
+//Math Methods
+
+//Number to Integer
+/*console.log(Math.round(4.6));
+console.log(Math.round(2.7));
+console.log(Math.round(9.8));
+console.log(Math.round(5.2));*/
+
+/*console.log(Math.ceil(4.9));
+console.log(Math.ceil(4.7));
+console.log(Math.ceil(4.4));
+console.log(Math.ceil(4.2));
+console.log(Math.ceil(-4.2));*/
+
+/*console.log(Math.floor(4.9));
+console.log(Math.floor(4.7));
+console.log(Math.floor(4.4));
+console.log(Math.floor(4.2));
+console.log(Math.floor(-4.2));*/
+
+/*console.log(Math.trunc(4.9));
+console.log(Math.trunc(4.7));
+console.log(Math.trunc(4.4));
+console.log(Math.trunc(4.2));
+console.log(Math.trunc(-4.2));*/
+
+//Math.sign()
+/*console.log(Math.sign(4));
+console.log(Math.sign(0));
+console.log(Math.sign(-4));*/
+
+//Math.pow()
+/*console.log(Math.pow(8, 2));*/
+
+//Math.sqrt()
+/*console.log(Math.sqrt(64));*/
+
+//Math.abs()
+/*console.log(Math.abs(-4.7));*/
+
+//Math.sin()
+/*console.log(Math.sin(90 * Math.PI /180));*/
+
+//Math.cos()
+/*console.log(Math.cos(0 * Math.PI /180));*/
+
+//Math.min() and Math.max()
+/*console.log(Math.min(0, 150, 30, 20, -8, -200));
+console.log(Math.max(0, 150, 30, 20, -8, -200));*/
+
+//The Math.log() Method
+/*console.log(Math.log(1));
+console.log(Math.log(2));
+console.log(Math.log(3));
+console.log(Math.log(10));*/
+
+//The Math.log2() Method
+console.log(Math.log2(8));
+
+//The Math.log10() Method
+console.log(Math.log10(1000));
