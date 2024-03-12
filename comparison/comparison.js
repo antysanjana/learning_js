cd2739491c4d2eb00144694eae7019624af2e16c
Anty <sanjana.anty@gmail.com>
2024-03-12 13:11:26 +0600

    Comparison

---
+++ comparison/comparison.js
@@ -0,0 +1,55 @@
+//Comparison Operators
+//let x = 5;
+
+//equal to
+/*console.log(x == 8);
+console.log(x == 5);
+console.log(x == "5");*/
+
+//equal value and equal type
+/*console.log(x === 5);
+console.log(x === "5");*/
+
+//not equal
+/*console.log(x != 8);
+console.log(x != "5");*/
+
+//not equal value or not equal type
+/*console.log(x !== "5");
+console.log(x !== 8);
+console.log(x !== 5);*/
+
+//Conditional (Ternary) Operator
+/*let age = 17;
+console.log(age >= 18 ? "You can vote" : "You are too young!");*/
+
+//Comparing Different Types
+/*console.log(2 < 12);
+console.log(2 < "12");
+console.log(2 < "John");
+console.log(2 > "John");
+console.log(2 == "John");
+console.log("2" < "12");
+console.log("2" > "12");
+console.log("2" == "12");*/
+
+//converting to proper type
+/*let age = "anty";
+
+age = Number(age);
+if(isNaN(age)){
+    console.log("Age is not a number");
+}
+else{
+    console.log(age > 18 ? "Too old!" : "Too young");
+}*/
+
+//The Nullish Coalescing Operator (??)
+/*let name_ = null;
+let text = "missing";
+console.log(name_ ?? text);*/
+
+//The Optional Chaining Operator (?.)
+/*const cars = {type: "Fiat", model: "500", color: "white"};
+console.log(cars?.name);*/
+
