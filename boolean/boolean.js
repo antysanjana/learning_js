609755479793e01fb183b97463a10b057de42324
Anty <sanjana.anty@gmail.com>
2024-03-12 12:45:01 +0600

    Boolean

---
+++ boolean/boolean.js
@@ -0,0 +1,27 @@
+// console.log(Boolean(10>9));
+// console.log(10>9);
+
+//Everything With a "Value" is True
+/*console.log(Boolean(100));
+console.log(Boolean(3.14));
+console.log(Boolean(-15));
+console.log(Boolean("Hello"));
+console.log(Boolean("False"));
+console.log(Boolean(7 + 1 + 3.14));*/
+
+//Everything Without a "Value" is False
+/*console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(false));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));*/
+
+//JavaScript Booleans as Objects
+let x = false;
+let y = new Boolean(false);
+console.log(typeof x);
+console.log(typeof y);
+
+
