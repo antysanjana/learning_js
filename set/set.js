e857dfa66f8edc159473e49a628f1277653a8fa5
Anty <sanjana.anty@gmail.com>
2024-03-13 19:38:33 +0600

    Set

---
+++ set/set.js
@@ -0,0 +1,32 @@
+//Create a Set
+/*const letters = new Set(["a", "b", "c"]);
+
+//Add values to the set
+letters.add("d");
+letters.add("d");
+letters.add("e");
+letters.add("f");
+
+console.log(letters);*/
+
+//The forEach() Method
+/*const letters = new Set(["a", "b" ,"c", "d"]);
+
+let text = "";
+letters.forEach(function(value, index, array){
+    text += value;
+})
+
+console.log(text);*/
+
+//The values() Method
+/*const letters = new Set(["a", "b" ,"c", "d"]);
+//console.log(letters.values());
+
+let text = "";
+for(let x of letters.values()){
+    text += x;
+}
+console.log(text);*/
+
+//
