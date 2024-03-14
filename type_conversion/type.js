eab78b77668e42d00bce7ef85168c09379ecd429
Anty <sanjana.anty@gmail.com>
2024-03-14 14:25:30 +0600

    Type Conversion

---
+++ type_conversion/type.js
@@ -0,0 +1,76 @@
+//Converting Strings to Numbers
+/*console.log(Number("3.14"));
+console.log(Number(Math.PI));
+console.log(Number(""));
+console.log(Number("    "));
+console.log(Number("99 89"));
+console.log(Number("John"));
+console.log(parseFloat("3.1416"));
+console.log(parseFloat("37"));
+console.log(parseInt("3.1416"));
+console.log(parseInt("39"));
+console.log(parseInt("Anty"));*/
+
+//The Unary + Operator
+/*let y = "5";
+let x = + y;
+console.log(x);
+
+let y = "John";
+let x = + y;
+console.log(x);*/
+
+//Converting Numbers to Strings
+/*let x = 123;
+console.log(String(x));
+console.log(String(123));
+console.log(String(100+23));
+console.log(String("123"));
+
+console.log(x.toString());
+console.log((123).toString());
+console.log((100+23).toString());*/
+
+/*let x = 10e5;
+console.log(x.toExponential());
+let y = 6.37878;
+console.log(y.toFixed());
+console.log(y.toFixed(2));
+console.log(y.toFixed(3));
+console.log(y.toPrecision(3));
+console.log(y.toPrecision(4));*/
+
+//Converting Dates to Numbers
+/*d = new Date();
+console.log(Number(d));
+
+d1 = new Date();
+console.log(d.getTime());*/
+
+//Converting Dates to Strings
+/*console.log(String(Date()));
+console.log(Date().toString());*/
+
+//Converting Booleans to Numbers
+/*console.log(Number(false));
+console.log(Number(true));
+console.log(String(true));
+console.log(String(false));
+console.log(false.toString());
+console.log(true.toString());*/
+
+//Automatic Type Conversion
+console.log(5 + null);
+console.log("5" + null);
+console.log(2 + "5");
+console.log("5" + "2");
+console.log("5" - 2);
+console.log("5" * 2);
+console.log("5" * "2");
+
+
+
+
+
+
+
