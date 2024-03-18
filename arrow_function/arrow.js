8d2890886d578375e43a83101debf697f41bc5f8
Anty <sanjana.anty@gmail.com>
2024-03-18 15:02:05 +0600

    arrow function

---
+++ arrow_function/arrow.js
@@ -0,0 +1,24 @@
+//Arrow function
+/*const hello = () => {
+    console.log("hello world");
+}
+
+hello();*/
+
+//Arrow Functions Return Value by Default:
+/*const hello = () => console.log("hello world");
+
+hello();*/
+
+//Arrow Function With Parameters:
+/*let val = "World!";
+
+const hello = (val) => console.log("Hello " + val)
+hello(val);*/
+
+//Arrow Function Without Parentheses:
+let val = "World!";
+
+const hello = val => console.log("Hello " + val)
+hello(val);
+
