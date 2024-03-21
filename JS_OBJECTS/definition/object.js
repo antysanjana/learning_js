386f45e9b9a0b6be6fdb3d63cae1a00b8c8eef18
Anty <sanjana.anty@gmail.com>
2024-03-21 12:19:59 +0600

    Defining Object

---
+++ JS_OBJECTS/definition/object.js
@@ -0,0 +1,32 @@
+//Creating a JavaScript Object
+
+//Using an Object Literal
+/*const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "Blue"
+};
+
+console.log(person);*/
+
+/*const person = {};
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = "50";
+person.eyeColor = "Blue";
+
+console.log(person);*/
+
+//Using the JavaScript Keyword new
+/*const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = "50";
+person.eyeColor = "Blue";
+
+console.log(person);*/
+
+//
+
+
